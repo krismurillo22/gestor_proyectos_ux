11db121626f932564e2f89d68e95ed8c7a507d15
Hector Carranza <hectorcarranza@MacBook-Air-de-Hector.local>
2026-05-20 17:07:57 -0600

    endpoint get clientes por id

--- Gestor/controllers/clientesController.js
+++ Gestor/controllers/clientesController.js
@@ -18,13 +18,37 @@ const getClientes = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+// GET /api/clientes/:id
+const getClienteById = async (req, res) => {
+  try {
+    const cliente = await Cliente.findByPk(req.params.id, {
+      include: [
+        {
+          model: TelefonoCliente,
+          as: 'telefonos',
+        },
+        {
+          model: Solicitud,
+          as: 'solicitudes',
+        },
+      ],
+    });
 
+    if (!cliente) {
+      return res.status(404).json({ error: 'Cliente no encontrado' });
+    }
+
+    res.json(cliente);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 
 module.exports = {
   getClientes,
-  /*getClienteById,
-  createCliente,
-  updateCliente,
-  deleteCliente,*/
+  getClienteById,
+  //createCliente,
+  //updateCliente,
+  //deleteCliente,
 };
