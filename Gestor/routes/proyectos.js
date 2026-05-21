a4f027958f78ea767c5e0bcd11008520e6c5e64e
Sofia <scastelar16@gmail.com>
2026-05-21 04:19:50 -0600

    correcion de ruta

--- Gestor/routes/proyectos.js
+++ Gestor/routes/proyectos.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const proyectosController = require('../controllers/proyectosController');
 
 router.get('/proyectos', proyectosController.getProyectos);
-router.get('/proyectos', proyectosController.getProyectoById);
+router.get('/proyectos/:id', proyectosController.getProyectoById);
 
 module.exports = router;
