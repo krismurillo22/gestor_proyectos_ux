655d9c0377305c6bfa92502be06ab8922cce4cc8
krismurillo22 <kristian_murillo@unitec.edu>
2026-05-28 08:29:11 -0600

    Agregacion de GET cotizaciones por proveedor

--- Gestor/routes/proveedores.js
+++ Gestor/routes/proveedores.js
@@ -7,5 +7,6 @@ router.get('/proveedores/:id', proveedoresController.getProveedorById);
 router.post('/proveedores', proveedoresController.createProveedor);
 router.put('/proveedores/:id', proveedoresController.updateProveedor);
 router.delete('/proveedores/:id', proveedoresController.deleteProveedor);
+router.get('/proveedores/:id/cotizaciones', proveedoresController.getCotizacionesByProveedor);
 
 module.exports = router;
